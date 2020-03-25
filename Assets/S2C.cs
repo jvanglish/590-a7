7da59e54b2a0e9ca5d0fabdc763843f2ff00f036
jvanglish <jordan.vanglish@gmail.com>
2020-03-25 00:47:40 -0400

    S2C cs file

---
+++ Assets/S2C.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public Camera camera;
+public Vector2 VRTrackingOrigin;
+
+public class S2C : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public angleBetweenVectors(Vector2 A, Vector2 B) {
+            return Math.Acos(Vecotr2.Dot(Vector2.Normalize(A), Vector2.Normalize(B)));
+        }
+        prevForwardVector = camera.transform.forward;
+        prevYawRelativeToCenter = angleBetweenVectors(camera.transform.forward, VRTrackingOrigin.transform.position - camera.transform.position);
+        public d(Vector2 A, Vector2 B, Vector2 C) {
+            return (A.x-B.x)*(C.y-B.y)-(A.y-B.y)*(C.x-B.x);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        howMuchUserRotated = angleBetweenVectors(prevForwardVector, camera.transform.forward);
+        directionUserRotated = (d(camera.transform.position+prevForwardVector, camera.transform.position, camera.transform.position + camera.transform.forward) < 0)?1:-1;
+        deltaYawRelativeToCenter = prevYawRelativeToCenter - angleBetweenVectors(camera.transform.forward, VRTrackingOrigin.transform.position - camera.transform.position);
+        distanceFromCenter = (camera.transform.position - VRTrackingOrigin.transform.position).magnitude;
+        longestDimensionOfPE = ???
+    }
+}
