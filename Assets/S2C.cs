baf1034719bef9c281a7801361bb19b20b187385
jvanglish <jordan.vanglish@gmail.com>
2020-03-26 22:20:24 -0400

    Nick helped

--- Assets/S2C.cs
+++ Assets/S2C.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class S2C : MonoBehaviour
 {    
     int longestDimensionOfPE;
-    float howMuchUserRotated, prevYawRelativeToCenter, directionUserRotated, deltaYawRelativeToCenter, distanceFromCenter;
-    double howMuchToAccelerate;
+    float howMuchUserRotated, prevYawRelativeToCenter, directionUserRotated, deltaYawRelativeToCenter, distanceFromCenter, howMuchToAccelerate;
+    //double howMuchToAccelerate;
     Vector3 prevForwardVector;
 
     public Camera camera;
@@ -25,17 +25,19 @@ public class S2C : MonoBehaviour
     {
         longestDimensionOfPE = 1;
         howMuchUserRotated = angleBetweenVectors(prevForwardVector, camera.transform.forward);
-        directionUserRotated = (direction(camera.transform.position+prevForwardVector, camera.transform.position, camera.transform.position + camera.transform.forward) < 0)?1:-1;
+        directionUserRotated = (direction(camera.transform.position+prevForwardVector, camera.transform.position, camera.transform.position + camera.transform.forward) < 0)?-1:1;
         deltaYawRelativeToCenter = prevYawRelativeToCenter - angleBetweenVectors(camera.transform.forward, VRTrackingOrigin.transform.position - camera.transform.position);
         distanceFromCenter = (camera.transform.position - VRTrackingOrigin.transform.position).magnitude;
         prevForwardVector=camera.transform.forward;
         prevYawRelativeToCenter=angleBetweenVectors(camera.transform.forward,VRTrackingOrigin.transform.position-camera.transform.position);
-        howMuchToAccelerate=((deltaYawRelativeToCenter<0)? - 0.13 : 0.30) * howMuchUserRotated * directionUserRotated * Mathf.Clamp(distanceFromCenter/longestDimensionOfPE/2,0,1);
-        VRTrackingOrigin.transform.RotateAround(camera.transform.position,(0,1,0),howMuchToAccelerate);
+        howMuchToAccelerate=((deltaYawRelativeToCenter<0)? - 0.13f : 0.30f) * howMuchUserRotated * directionUserRotated * Mathf.Clamp(distanceFromCenter/longestDimensionOfPE/2,0,1);
+        if(Mathf.Abs(howMuchToAccelerate) > 0) {
+            VRTrackingOrigin.transform.RotateAround(camera.transform.position, new Vector3(0.0f, 1.0f, 0.0f), (float)howMuchToAccelerate);
+        }
     } 
 
-    float angleBetweenVectors(Vector2 A, Vector2 B) {
-        return Math.Acos(Vector2.Dot(Vector2.Normalize(), Vector2.Normalize()));
+    float angleBetweenVectors(Vector3 A, Vector3 B) {
+        return Mathf.Acos(Vector3.Dot(A.normalized, B.normalized))*180/Mathf.PI;
     }
 
     float direction(Vector3 A, Vector3 B, Vector3 C) {
